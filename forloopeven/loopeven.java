312fb5921199fe932ed108dd8a4f9ccd06e80082
prakash102006 <balaprakash553@gmail.com>
2025-03-13 14:21:46 +0530

    Forloop Even

---
+++ forloopeven/loopeven.java
@@ -0,0 +1,9 @@
+public class loopeven{
+    public static void main(String args[]) {
+        for(int i = 11; i < 21; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
