d3d24eb71f03c60a28024f0555fae37bcd014fd4
prakash102006 <balaprakash553@gmail.com>
2025-03-13 10:41:19 +0530

    Conditional Operator

---
+++ conditionaloperator/Conditionaloperator.java
@@ -0,0 +1,9 @@
+public class Conditionaloperator{
+    public static void main(String args[]){
+        System.out.println(10>2);
+        System.out.println(1<8);
+        System.out.println(6!=3);
+        System.out.println(7>=2);
+        System.out.println(8<=9);
+    }
+}
