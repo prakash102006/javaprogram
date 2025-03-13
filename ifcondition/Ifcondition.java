57e786d62ab661ca545b97c3f170b7490b4db21d
prakash102006 <balaprakash553@gmail.com>
2025-03-13 10:54:36 +0530

    if else

---
+++ ifcondition/Ifcondition.java
@@ -0,0 +1,10 @@
+public class Ifcondition {
+    public static void main(String[] args) {
+        int number = 10;
+        if (number % 2 == 0) { 
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+    }
+}
