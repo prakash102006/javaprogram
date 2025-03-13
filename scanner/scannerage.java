3adb7c289248c71ef6c4aafff78077ee679de3d4
prakash102006 <balaprakash553@gmail.com>
2025-03-13 15:23:45 +0530

    Enter ur age

---
+++ scanner/scannerage.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class scannerage{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your age");
+        int age = input.nextInt();
+        System.out.println( age);
+    }
+}
