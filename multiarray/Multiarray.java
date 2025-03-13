36901864fc5bed04cf25e7e7d6851b90a7ee0201
prakash102006 <balaprakash553@gmail.com>
2025-03-13 13:46:52 +0530

    Multi Dimesional Array

---
+++ multiarray/Multiarray.java
@@ -0,0 +1,7 @@
+public class  Multiarray{
+    public static void main(String args[]){
+        int arr[][]={{2,3,4,5,6,7},{7,8,5,3,4,9},{1,8,6,3,9,0}};
+        System.out.println(arr);
+        System.out.println(arr[0][1]);
+    }
+}
