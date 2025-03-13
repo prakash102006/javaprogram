da38e628bb12770890d97f44a20d250509e859d1
prakash102006 <balaprakash553@gmail.com>
2025-03-13 11:17:42 +0530

    positive negative

---
+++ positive/positive.java
@@ -0,0 +1,10 @@
+public class positive {
+    public static void main(String[] args) {
+        int number = 19;
+        if (number>=0) { 
+            System.out.println("The given number is positive");
+        } else  {
+            System.out.println("The given number is negative");
+        }
+    }
+}
