c1f754390109dcce73c6def4dca0ab8b09d09bea
prakash102006 <balaprakash553@gmail.com>
2025-03-13 14:33:21 +0530

    leap year

---
+++ leapyear/leapyear.java
@@ -0,0 +1,10 @@
+public class leapyear {
+    public static void main(String args[]) {
+        int year = 2022;
+        if (year % 4 == 0){
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
