740b9084f438fb2a507239992e3aa4cb893f1522
prakash102006 <balaprakash553@gmail.com>
2025-03-13 14:25:41 +0530

    For loop odd

---
+++ forloopodd/loopodd.java
@@ -0,0 +1,9 @@
+public class loopodd {
+    public static void main(String args[]) {
+        for (int i = 11; i < 21; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
