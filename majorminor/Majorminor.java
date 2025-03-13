181b78177fd78d500f157f466d7d3543a0f713fd
prakash102006 <balaprakash553@gmail.com>
2025-03-13 11:09:47 +0530

    majorminor

---
+++ majorminor/Majorminor.java
@@ -0,0 +1,10 @@
+public class Majorminor {
+    public static void main(String[] args) {
+        int age = 19;
+        if (age>=18) { 
+            System.out.println("Yor are major");
+        } else  {
+            System.out.println("You are minor");
+        }
+    }
+}
