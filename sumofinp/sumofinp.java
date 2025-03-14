4a462fb1ff218ff3bb2ded2fdb5918a6d6402832
prakash102006 <balaprakash553@gmail.com>
2025-03-14 15:37:08 +0530

    Sum of Multiple Input

---
+++ sumofinp/sumofinp.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class sumofinp {
+    public static void main(String[] args)
+    {
+        int i=0;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter the number of digit:");
+        int n=obj.nextInt();
+        int a[]=new int[n];
+        int sum=0;
+        while(i<n){
+            System.out.println("enter the number :");
+             a[i]=obj.nextInt();
+             sum =sum+a[i];
+            i++;
+        }
+       
+        
+        System.out.println("the addition is:" +sum);    
+    
+    }
+}
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
