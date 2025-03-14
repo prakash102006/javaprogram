ceb5737223a63245417b43fd7c7a74c6221ba948
prakash102006 <balaprakash553@gmail.com>
2025-03-14 13:15:58 +0530

    name length

---
+++ split/split.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class split {
+    public static void main(String args[]) {
+        String name = "hello world";
+        String names[] = name.split(" ");
+        System.out.println(name.length());
+    }
+}
