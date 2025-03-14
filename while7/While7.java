853b1b87ad85dddcf56724a17152d9c13710d0f6
prakash102006 <balaprakash553@gmail.com>
2025-03-14 10:19:03 +0530

    7 Table

---
+++ while7/While7.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class While7{
+    public static void main (String args []){
+        int i = 0;
+        int value =0;
+        while(i<10){
+            i=i+1;
+            value=7*i;
+            System.out.println(value);
+        }
+    }
+}
