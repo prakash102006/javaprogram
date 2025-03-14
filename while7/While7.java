a898b83eb6d9e11cd55fdc98ad10f38dadd50279
prakash102006 <balaprakash553@gmail.com>
2025-03-14 10:30:03 +0530

    7 Table

--- while7/While7.java
+++ while7/While7.java
@@ -6,7 +6,7 @@ public class While7{
         while(i<10){
             i=i+1;
             value=7*i;
-            System.out.println(value);
+            System.out.println("7*"+i+"="+value);
         }
     }
 }
