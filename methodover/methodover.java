aae5e7ba6515a9145422c2c791100eb2e273a0b9
prakash102006 <balaprakash553@gmail.com>
2025-03-14 14:23:29 +0530

    Method over writing

---
+++ methodover/methodover.java
@@ -0,0 +1,16 @@
+class Parent {
+    public void add() {
+        System.out.println("hello world");
+    }
+}
+class Child extends Parent {
+    void display() {
+        System.out.println("home");
+    }
+}
+public class methodover{
+    public static void main (String args []){
+        Child obj = new Child();
+        obj.display();
+    }
+}
