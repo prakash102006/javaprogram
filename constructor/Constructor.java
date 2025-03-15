e53856e3947fb27323c97959e0d4a430119f382b
prakash102006 <balaprakash553@gmail.com>
2025-03-15 10:44:06 +0530

    Constructor

---
+++ constructor/Constructor.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Constructor{
+
+    Constructor(){
+        System.out.println("Heisenberg");
+    }    
+    public static void main(String args[]){
+        Constructor obj = new Constructor ();
+    }
+}
